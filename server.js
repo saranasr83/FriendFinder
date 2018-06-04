e5dbfb0b6a97de55abbd9229dcc5b99b2a393419
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-06-04 16:12:07 -0700

    update server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('./app/public'));
+
+// routes to be used by server
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
