6adb5a60c8ce1ba3b52c52289275ec1faaee3657
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-06-04 16:56:29 -0700

    update html routes and html files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,22 @@
 var friendData = require('../data/friends');
 
 module.exports = function(app){
+
     app.get("/api/friends", function(req, res){
         res.json(friendData);
     });
 
     app.post("/api/friends", function(req, res){
        // all logic goes here
+
+       // save the newest friend to a variable 
+       var friendToBeMatched =  req.body; // form data from html 
+       var bestScore = 1000;
+       var newMatch;
+       // loop through all existing friends in friendData array
+       // var current friend in loop loop through their score and do compararison of each score
+       // for the first loop it
+
+       res.json(newMatch);
     });
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,4 +2,18 @@ var path = require('path');
 
 module.exports = function(app){
     
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    
+    app.get("*", function(req, res) {
+        if(req.url.indexOf('/api') == 0) return next();
+        if(req.url.indexOf('/assets') == 0) return next();
+        if(req.url.indexOf('/css') == 0) return next();
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 }
